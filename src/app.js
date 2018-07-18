e5823d9e9dd0f46020c69f4455b3234904af0548
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-07-18 04:47:19 +0100

    [Chores #159117716] Setup babel configuration to get browser-compatible javascript

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import indexRouter from './api/routes/index';
+
+// Declare an app from express
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.use('/', indexRouter);
+
+export default app;
