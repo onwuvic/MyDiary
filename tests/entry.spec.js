7ccd5057a8d68319bff0659e7c2206d59c537ce8
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-07-20 17:18:48 +0100

    [Feature #159114881] Create and test new entry endpoint

--- tests/entry.spec.js
+++ tests/entry.spec.js
@@ -1,9 +1,12 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import uniqid from 'uniqid';
 import app from '../src/app';
 
 chai.use(chaiHttp);
 let baseUrl = '/api/v1';
+let id = uniqid();
+let newEntry = {id: id, title: 'play of all', body: 'He plays all time', feature_image: 'http://image.jpg', status: 1};
 
 describe("Entry", () => {
   describe("entries endpoints", () => {
@@ -22,5 +25,25 @@ describe("Entry", () => {
           });
       });
     });
+
+    describe("POST /entries", () => {
+      it("should CREATE a new entry", (done) => {
+        chai.request(app)
+          .post(`${baseUrl}/entries`)
+          .send(newEntry)
+          .end((error, res) => {
+            if(error) done(error);
+
+            expect(res).to.have.status(201);
+            expect(res).to.be.json;
+            expect(res.body).to.have.property('title').eql('play of all');
+            expect(res.body).to.have.property('body').eql('He plays all time');
+            expect(res.body).to.have.property('feature_image').eql('http://image.jpg');
+            expect(res.body).to.have.property('status').eql(1);
+            done();
+          });
+      });
+    });
+
   });
 });
