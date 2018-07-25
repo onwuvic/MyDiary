e9c3ea66ed9329c367fcce59b0a85b96f2b421d6
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-07-25 21:36:50 +0100

    [Feature #158910236] Adjust UI from feedback

--- UI/javascript/mydiary.js
+++ UI/javascript/mydiary.js
@@ -2,6 +2,9 @@ const close = document.getElementById('close-menu');
 const open = document.getElementById('open-menu');
 const sideMenu = document.getElementById('side-menu');
 const main = document.getElementById('main');
+let moreInfo = document.querySelectorAll('#more-info');
+let moreContent = document.querySelector('#more-content');
+let moreInformation = document.querySelector('#more-information');
 
 open.onclick = () => {
     sideMenu.style.width = '250px';
@@ -12,3 +15,17 @@ close.onclick = () => {
     sideMenu.style.cssText = "";
     main.style.cssText = "";    
 }
+
+function toggleOpen() {
+    this.parentNode.classList.toggle('clicked');
+}
+ 
+function display() {
+    moreContent.classList.toggle('clicked');
+}
+ 
+if (moreInformation) {
+    moreInformation.addEventListener('click', display, false);  
+}
+ 
+moreInfo.forEach(panel => panel.addEventListener('click', toggleOpen, false));
