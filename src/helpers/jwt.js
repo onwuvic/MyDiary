7641c65b439547d9aab3adee846a5dbf54828d66
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-07-27 21:54:29 +0100

    Feature (user login endpoint): Create and test login endpoint

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -1,5 +1,14 @@
 import jwt from 'jsonwebtoken';
 
+/**
+ * A function that generate web token.
+ *
+ * @param {Request: user input request} req
+ * @param {Response: programs reponses} res
+ * @param {Next: move on to the next middleware } next
+ *
+ * return a JsonWebToken string
+ */
 const tokenGenerator = (object) => {
   return jwt.sign(object, process.env.SECRET_KEY, {
     expiresIn: process.env.TOKEN_EXPIRATION
--- src/middlewares/isAnyEmpty.js
+++ src/middlewares/isAnyEmpty.js
@@ -1,7 +1,18 @@
+/**
+ * A function that check if sign up input is empty.
+ *
+ * @param {Request: user input request} req
+ * @param {Response: programs reponses} res
+ * @param {Next: move on to the next middleware } next
+ *
+ * return status: 400 [bad request] 'All fields are required' or move to next middleware
+ */
 const isAnyEmpty = (req, res, next) => {
+
   const { firstname, lastname, email, password } = req.body;
-  if(!firstname || !lastname || !email || !password) {
-    return res.status(422).send('All fields are required');
+
+  if(!firstname.trim() && !lastname.trim() && !email.trim() && !password.trim()) {
+    return res.status(400).send('All fields are required');
   } else {
     return next();
   }
