aaf382f07bd27f5d3c8cf70a7c26e0cd09efb358
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-07-28 13:06:11 +0100

    Feature (authentication method): Add a method that will secure all routes

--- src/contollers/UserController.js
+++ src/contollers/UserController.js
@@ -1,6 +1,7 @@
 import db from '../database';
 import tokenGenerator from '../helpers/jwt';
 import bcrypt from 'bcrypt';
+import refactorUserData from '../helpers/refactorUserData';
 
 
 /**
@@ -11,11 +12,12 @@ import bcrypt from 'bcrypt';
  * return web token [which only user to navigate secure part of the app and resources ]
 */
 export const signUp = (req, res) => {
-  db.one('insert into users(firstname, lastname, email, password)' +
-  'values(${firstname}, ${lastname}, ${email}, ${password}) returning *',
+  db.one('INSERT INTO users(firstname, lastname, email, password)' +
+  'VALUES(${firstname}, ${lastname}, ${email}, ${password}) RETURNING *',
     req.body)
-    .then((result) => {
-      const token = tokenGenerator(result);
+    .then((user) => {
+      const userData = refactorUserData(user);
+      const token = tokenGenerator(userData);
       return res.status(201).json(token);
     })
     .catch((err) => res.status(500).send('Can not sign up now. Try again.'));
@@ -39,7 +41,9 @@ export const logIn = (req, res) => {
   // check if mail exist in the database
   db.one('SELECT * FROM users WHERE email = $1', email)
     .then(user => {
-      let userData = user;
+      const userData = user;
+      // refactor the user data to exclude password
+      const refactorUser = refactorUserData(user);
       if (userData) {
         // check if user password match
         bcrypt.compare(password, userData.password)
@@ -47,7 +51,7 @@ export const logIn = (req, res) => {
           if (!matched) return res.status(400).send('Invalid email and password.');
         })
         .then(() => {
-          const token = tokenGenerator(userData);
+          const token = tokenGenerator(refactorUser);
           return res.status(200).json(token);
         })
         .catch((error) => res.status(505).send('Can not resolve password'));
