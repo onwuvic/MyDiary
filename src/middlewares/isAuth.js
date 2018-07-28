9a41e8bf8789e4db13c2e35bbed7b8f3c789c303
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-07-28 13:06:11 +0100

    Feature (authentication method): Add a method that will secure all routes

---
+++ src/middlewares/isAuth.js
@@ -0,0 +1,58 @@
+import jwt from 'jsonwebtoken';
+import db from './../database';
+import refactorUserData from '../helpers/refactorUserData';
+
+/**
+ * A function that check if the token provided is correct and if the user is authenticated.
+ *
+ * @param {Request: user input request} req
+ * @param {Response: programs reponses} res
+ * @param {Next: move on to the next middleware } next
+ *
+ * return boolean: true if valid or status code of specific error if not valid
+ */
+const isAuth = (req, res, next) => {
+  let bearerToken;
+  // get the req.header authorization token
+  let bearerHeader = req.header["authorization"];
+
+  if(typeof bearerHeader !== 'undefined') {
+    let bearer = bearerHeader.split(' ');
+    bearerToken = bearer[1];
+
+    // check if the bearer match that of the bearer specified in our env
+    if(bearer[0] !== process.env.BEARER) {
+      return res.status(400).send('bearer not understood');
+    }
+
+    // used jsonwebtoken to decode and very if the token is valid
+    jwt.verify(bearerToken, process.env.SECRET_KEY, (err, decoded) => {
+      if(err) {
+        if(err.name === 'TokenExpiredError') {
+          return res.status(400).send('Session timed out, please login again');
+        } else {
+          return res.status(400).send('Error authenticating, please login again');
+        }
+      }
+
+      // get the authenticated user data from the database
+      db.one('SELECT * FROM users WHERE id = $1', decoded.id)
+        .then((user) => {
+          // refactor the user data to exclude user password
+          let userData = refactorUserData(user);
+
+          // assign the user data into the req params for used in the next call
+          req.user = userData;
+          // move to next middleware or function
+          next();
+        })
+        .catch(() =>
+          res.status(404).send('User not found')
+        );
+    });
+  } else {
+    return res.status(400).send('No token provided');
+  }
+}
+
+export default isAuth;
