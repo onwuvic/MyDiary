48d86d4cb82dfd1ed9aef56bba3273e752bf2bf7
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-07-30 01:54:33 +0100

    Feature(delete diary endpoint): Create delete diary endpoint with test suits

--- src/contollers/EntryController.js
+++ src/contollers/EntryController.js
@@ -26,10 +26,10 @@ export const findOneById = (req, res) => {
 
   db.one('SELECT * FROM entries WHERE id = $1 AND users_id = $2', [entryId, req.user.id])
     .then((entries) => res.status(200).json(entries))
-    .catch(() => res.status(404).send('No diary found with this ID'));
+    .catch(() => res.status(404).send('None of your diaries was found with this ID'));
 };
 
-// // UPDATE A SINGLE ENTRY IN MEMORY
+// // UPDATE A SINGLE ENTRY
 // export const updateOne = (req, res) => {
 //   Entry.findByIdAndUpdate(req.params.id,
 //     {
@@ -40,3 +40,13 @@ export const findOneById = (req, res) => {
 //     .then(entry => res.status(200).json(entry))
 //     .catch(() => res.status(500).send('There was a problem updating the entry.'));
 // };
+
+
+// DELETE A SINGLE ENTRY
+export const findByIdAndRemove = (req, res) => {
+  const entryId = parseInt(req.params.id);
+
+  db.result('DELETE FROM entries WHERE id = $1 AND users_id = $2', [entryId, req.user.id])
+    .then((entries) => res.status(200).send('Diary was deleted successfully!!!'))
+    .catch(() => res.status(500).send('There was a problem deleting the entry.'));
+};
