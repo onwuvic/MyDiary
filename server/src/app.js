2f2eb1a7d625ebf48ea3cf118fa54effb5ae3cab
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-08-02 11:59:55 +0100

    Bug(CORS ERROR): Fix CORS ERROR

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import indexRouter from './routes/index';
 
 // Declare an app from express
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
