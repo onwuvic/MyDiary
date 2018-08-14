4351a6933be0aa6cf8b26bf632b0d66b991f2cf9
Onwuzor Victor <onwuzorvictor@outlook.com>
2018-08-14 16:41:19 +0100

    chore(swagger doc): Add swagger documentation of the API

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 import cors from 'cors';
 import indexRouter from './routes/index';
 
 // Declare an app from express
 const app = express();
 
+const name = path.join(__dirname, '../../swagger');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 app.use('/api/v1', indexRouter);
+app.use('/api/v1/documentation', express.static(name));
 
 
 app.use((error, req, res, next) => {
